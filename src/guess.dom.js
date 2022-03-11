b419b72fcc8c506c4a72bb8a2cb93a3d1f59e4ed
Sinovuyo <cnovuyo10@gmail.com>
2022-03-11 15:03:14 +0200

    added the new game message

--- src/guess.dom.js
+++ src/guess.dom.js
@@ -6,94 +6,64 @@ let randomNumber = Math.ceil((Math.random() * 100));
 
 
 
- function getMessage() {
+function getMessage() {
     const userNumber = userInput.value;
+
+    console.log({ userNumber, randomNumber });
 
     if (userNumber == randomNumber) {
 
-       return `Correct, the secret number is ${userNumber}`;
+        return `Correct, the secret number is ${userNumber}`;
     }
 
-   if (userInput.value == "") {
-         return `Please guess a number between 1 and 100`;
+    if (userInput.value == "") {
+        return `Please guess a number between 1 and 100`;
     }
 
-    if((userNumber < 1)  || (userNumber > 100)) {
+    if ((userNumber < 1) || (userNumber > 100)) {
         return `Number ${userNumber} is out of range!`;
     }
 
-    if(userNumber > randomNumber) {
+    if (userNumber > randomNumber) {
         return `Number ${userNumber} is too high!`;
     }
 
-  if(userNumber < randomNumber) {
+    if (userNumber < randomNumber) {
         return `Number ${userNumber} is too low!`;
     }
 
 }
 
-async function theTime() {
-    await new Promise(resolve => setTimeout(resolve, 5000));
-     location.reload();
+function NewGame() {
+    setTimeout(() => {
+        randomNumber = Math.ceil((Math.random() * 100));
+        message.innerText = `A new game has started`;
+
+    }, 5000)
 }
 
-userInput.addEventListener('keyup', function(){
-    if(event.keyCode === 13) {
+userInput.addEventListener('keyup', function () {
+    if (event.keyCode === 13) {
         // event.preventDefault();
-        if(getMessage().charAt(0).toLowerCase() == `c`) {
+        if (getMessage().charAt(0).toLowerCase() == `c`) {
             message.innerText = getMessage();
-            theTime();
+            // theTime();
+            NewGame();
+
         } else {
             message.innerText = getMessage();
         }
     }
 })
 
-guessBtn.addEventListener('click', function(){
-    if(getMessage().charAt(0).toLowerCase() == `c`) {
+guessBtn.addEventListener('click', function () {
+    if (getMessage().charAt(0).toLowerCase() == `c`) {
         message.innerText = getMessage();
-        // theTime();
+        //    theTime();
+        NewGame();
     } else {
         message.innerText = getMessage();
     }
 })
 
 
-/** 
-async function guessCheck(ranNo) {
-    while (true) {
-
-        //    const userInput = prompt("guess number")
-        //  alert(randomNumber);
-
-
-        guessBtn.addEventListener("click", function () {
-
-            if (userInput.value == "") {
-                message.innerHTML = "Please guess a number";
-                return;
-            }
-
-
-            if (ranNo == userInput) {
-                message.innerHTML = `Correct, the secret number is ${ranNo}`;
-                await new Promise(resolve => setTimeout(resolve, 1000));
-                location.reload();
-                return false;
-            }
-
-            else if (userInput > ranNo) {
-                message.innerHTML = `Your guess is too high`;
-            }
-
-            else if (userInput < ranNo) {
-                message.innerHTML = `Your guess is too low`;
-            }
-        }
-
- 
-    
-     }
-}
-
-*/
